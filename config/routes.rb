b9b4bf128410f004cac7e7e7af5198c654a40c8d
temiooo <temitope.joloko@gmail.com>
2019-02-12 17:39:07 +0100

    change root route to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "user_files#index"
   resources :user_files, except: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
