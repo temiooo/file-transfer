7d2361446523e05d7c52cc08c2fff67d3dabf197
temiooo <temitope.joloko@gmail.com>
2019-02-19 18:56:38 +0100

    add validation to check for presence of attached file

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,3 +1,9 @@
 class UserFile < ApplicationRecord
   has_one_attached :user_file
+
+  validate :user_file?
+
+  def user_file?
+    errors[:base] << 'Please attach a file to upload' unless user_file.attached?
+  end
 end
